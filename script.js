f2d65ad93ae448920a3d4a77fc350a4e4924e6cb
Jessica Maple <maple@jessicas-mbp-4.attlocal.net>
2020-04-08 08:44:17 -0400

    updated html

---
+++ script.js
@@ -0,0 +1,12 @@
+/* 1.  Start button 
+ 2. high score list 
+3. timer 
+4. Questions 
+5. If correct, move to next question
+6. If incorrect, 15 seconds are decucted 
+7. Starts from 75 seconds
+8. each question gets 15 seconds
+9. When time runs out/ or all questions are answered, user is prompted to enter their initials. 
+10. Score and initials are stored in Local Storage 
+*/ 
+
